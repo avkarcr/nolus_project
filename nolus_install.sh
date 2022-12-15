477981188a2049bee3ea38755380843d87bc92fe
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2022-12-15 21:15:48 +0300

    Update nolus_install.sh
    Added node registration block

--- nolus_install.sh
+++ nolus_install.sh
@@ -26,3 +26,48 @@ else
     echo "======================================"    
 fi
 echo
+read -p "Input your Node moniker: " moniker
+nolusd init $moniker
+wget https://raw.githubusercontent.com/Nolus-Protocol/nolus-networks/main/testnet/nolus-rila/genesis.json
+mv ./genesis.json ~/.nolus/config/genesis.json
+PEERS="$(curl -s "https://raw.githubusercontent.com/Nolus-Protocol/nolus-networks/main/testnet/nolus-rila/persistent_peers.txt")"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" ~/.nolus/config/config.toml
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025unls\"/" ~/.nolus/config/app.toml
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@latest
+tee <<EOF >> ~/.bashrc
+export DAEMON_NAME=nolusd
+export DAEMON_HOME=$HOME/.nolus
+export MONIKER_NAME=$moniker
+EOF
+. ~/.bashrc
+mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
+mkdir -p $DAEMON_HOME/cosmovisor/upgrades
+cp ~/go/bin/nolusd $DAEMON_HOME/cosmovisor/genesis/bin
+tee <<EOF > /etc/systemd/system/cosmovisor.service
+[Unit]
+Description=cosmovisor
+After=network-online.target
+
+[Service]
+User=root
+ExecStart=/root/go/bin/cosmovisor run
+Restart=always
+RestartSec=3
+LimitNOFILE=4096
+Environment="DAEMON_NAME=nolusd"
+Environment="DAEMON_HOME=/root/.nolus"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="DAEMON_LOG_BUFFER_SIZE=512"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable cosmovisor
+systemctl start cosmovisor
+echo "curl http://localhost:26657/status | jq .result.sync_info.catching_up" > ~/nolus_check_status.sh && chmod +x ~/nolus_check_status.sh
+echo
+echo "==================================================================================================="    
+echo -e "\e[1;32m Готово! Проверить работает ли нода можно с помощью скрипта nolus_check_status.sh \e[0m"
+echo "==================================================================================================="    
