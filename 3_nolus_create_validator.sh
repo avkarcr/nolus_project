9ff74503a7ad319738b3d5df607fe26078277ea3
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2022-12-27 15:23:27 +0300

    Update 3_nolus_create_validator.sh
    Changed the formula

--- 3_nolus_create_validator.sh
+++ 3_nolus_create_validator.sh
@@ -3,24 +3,12 @@
 echo
 echo "Before creating the Validator node you SHOULD wait until the Node is syncronized"
 echo "Please check that the Node catching up status is FALSE"
-echo "To exit press ctrl+c"
-echo "To continue just wait for 5 sec"
 echo
-sleep 5
-nolusd tx staking create-validator \
---amount=1000000unls \
---pubkey=$(nolusd tendermint show-validator) \
---moniker=$MONIKER \
---chain-id=nolus-rila \
---commission-rate=0.05 \
---commission-max-rate=0.20 \
---commission-max-change-rate=0.01 \
---min-self-delegation=1 \
---from=wallet \
---gas-adjustment=1.4 \
---gas=auto \
---fees=1000unls \
--y
+echo "To exit press ctrl+c"
+echo "To continue just wait for 10 sec"
+echo
+sleep 10
+nolusd tx staking create-validator --amount=1000000unls --pubkey=$(nolusd tendermint show-validator) --moniker=$MONIKER --chain-id=nolus-rila --commission-rate=0.05 --commission-max-rate=0.20 --commission-max-change-rate=0.01 --min-self-delegation=1 --from=wallet --gas-adjustment=1.4 --gas=auto --fees=1000unls -y
 sleep 2
 nolusd tx bank send wallet $ADDRESS 500000unls --from wallet --chain-id nolus-rila --fees=1000unls
 echo
